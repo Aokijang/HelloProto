e27202462357065fef2eb0322e224bb5b98cb479
Wendy Yansah <wendycoolzzz74@gmail.com>
2018-01-27 09:40:35 +0700

    Perubahan file, grid, index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-12">
+  				<div class="row">
+  					<div class="col-6">
+  						<img src="">
+  					</div>
+  					<div class="col-6 nav-bar">
+  						<ul>
+  							<li><a href="">...</a></li>
+  							<li><a href="">...</a></li>
+  							<li><a href="">...</a></li>
+  						</ul>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  </body>
+</html>
