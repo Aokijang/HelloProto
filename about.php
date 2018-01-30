5982ce0204ec3a2c397c6cf7268a8900ead2258b
Vanestica Ireline <virelinez@gmail.com>
2018-01-31 05:24:37 +0700

    responsive

--- about.php
+++ about.php
@@ -64,11 +64,10 @@
             <div class="col-10 content">
               <div class="row">
                 <div class="col-12">
-<<<<<<< HEAD
+                  
                   <div class="text-cont other-text-cont">
-=======
+
                   <div class="text-cont">
->>>>>>> 8913b399d5ed54558b86b3dbc458781e492698e9
                     <h1 class="title-text">About</h1>
                     <div class="other-text-cont">
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
