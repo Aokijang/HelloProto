18b4e91358a41ebcfa4a79f71d156e8597e31e89
Aokijang <balebodoh@gmail.com>
2018-01-31 11:44:02 +0700

    lala

--- about.php
+++ about.php
@@ -50,8 +50,8 @@
           <div class="row">
             <div class="col-2 bg_sidebar">
               <ul class="navbar">
-                <li class="hover"><a href="siswa.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
+                <li><a href="siswa.php">Home</a></li>
+                <li class="hover"><a href="about.php">About</a></li>
                 <li><a href="help.php">Help</a></li>
                 <li><a href="class.php">Class</a></li>
                 <li><a href="note.php">Note</a></li>
@@ -64,39 +64,11 @@
             <div class="col-10 content">
               <div class="row">
                 <div class="col-12">
-                  <div class="text-cont">
-                    <div class="row">
-                      <div class="col-1">
-                        <div class="text-img">
-                          <img src="css/profile.png" alt="">
-                        </div>
-                      </div>
-                      <div class="col-10">
-                        <div class="text-name">
-                          <h1>Wendy Yansah</h1>
-                        </div>
-                      </div>
-                      <div class="col-1"></div>
-                    </div>
-                    <hr class="text-line">
-                    <div class="row">
-                      <div class="col-1"></div>
-                      <div class="col-10 text-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                      </div>
-                      <div class="col-1"></div>
-                    </div>
-
-                    <div class="row">
-                      <div class="text-button">
-                        <div class="col-8"></div>
-                        <div class="col-2">
-                          <button type="button" name="like" id="like">Like</button>
-                        </div>
-                        <div class="col-2">
-                          <button type="button" name="reply" id="reply">Reply</button>
-                        </div>
-                      </div>
+                  <div class="text-cont other-text-cont">
+                    <h1 class="title-text">About</h1>
+                    <div class="other-text-cont">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                   </div>
                 </div>
---
+++ discussion.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-12 bg_navbar">
+  				<div class="row">
+  					<div class="col-6">
+  						<div class="row icon">
+  							<div class="col-2" align="right">
+  								<img src="css/magnifying-glass-flat-icons.svg">
+  							</div>
+  							<div class="col-10">
+  								<div class="title">
+  									<h3>HelloWorld!</h3>
+  								</div>
+  								<div class="subtitle">
+  									<p>Saatnya belajar</p>
+  								</div>
+  							</div>
+  						</div>
+  					</div>
+  					<div class="col-6 nav-bar">
+              <div class="row">
+                <div class="col-11 username">
+                  <p>Wendy Yansah</p>
+                  <div class="logout">
+                    <a class="keluarout" href="index.php">Keluar</a>
+                  </div>
+
+                </div>
+                <div class="col-1 logo" style="text-align:right">
+                  <img src="css/profile.png">
+                </div>
+
+
+              </div>
+   					</div>
+  				</div>
+  			</div>
+  		</div>
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-2 bg_sidebar">
+              <ul class="navbar">
+                <li><a href="siswa.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="help.php">Help</a></li>
+                <li><a href="class.php">Class</a></li>
+                <li><a href="note.php">Note</a></li>
+                <li><a href="assignment.php">Assignment</a></li>
+                <li><a href="quiz.php">Quiz</a></li>
+                <li class="hover"><a href="discussion.php">Discussion</a></li>
+                <li><a href="task.php">Task List</a></li>
+              </ul>
+            </div>
+            <div class="col-10 content">
+              <div class="row">
+                <div class="col-12">
+                  <div class="other-text-cont note-cont">
+                    <h1 class="title-text">X TKJ 2</h1>
+                    <div class="chat-room">
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="chat-cont">
+                            <div class="row">
+                              <div class="col-8">
+                                <div class="chat-text-cont">
+                                  <div class="row">
+                                    <div class="col-2">
+                                      <img src="css/profile3.png" alt="">
+                                    </div>
+                                    <div class="col-10">
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <h2>Vanestica Ireline</h2>
+                                        </div>
+                                      </div>
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <div class="chat-text">
+                                            Harap Di Kerjakan Quiznya
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="col-4"></div>
+                            </div>
+                          </div>
+
+                          <div class="chat-cont">
+                            <div class="row">
+                              <div class="col-4"></div>
+                              <div class="col-8">
+                                <div class="chat-text-cont">
+                                  <div class="row">
+                                    <div class="col-10 text-me">
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <h2>Kamu</h2>
+                                        </div>
+                                      </div>
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <div class="chat-text text-me-border">
+                                            Ok bu
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div class="col-2">
+                                      <img src="css/profile.png" alt="">
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="chat-input">
+                        <div class="col-10">
+                          <input type="text" name="send-text" id="send-text" placeholder="Masukkan pesan anda disini...">
+                        </div>
+                        <div class="col-2">
+                          <input type="submit" name="send" id="send" value="Kirim">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div class="row">
+          <div class="col-5 repad">
+            <div class="row">
+              <div class="col-12">
+                <p>Have any tips for us? Click this button.</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <a href="#">Ideas and Tips</a>
+              </div>
+            </div>
+            <div class="row" id="footer-menu-container">
+              <div class="col-3 footer-menu">
+                FAQ
+              </div>
+              <div class="col-3 footer-menu">
+                About Us
+              </div>
+              <div class="col-3 footer-menu">
+                Contact Us
+              </div>
+              <div class="col-3"></div>
+            </div>
+          </div>
+          <div class="col-7"></div>
+        </div>
+      </footer>
+  	</div>
+  </body>
+</html>
