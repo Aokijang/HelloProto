b4cfbc5b0de8b9e0310f29b6876733c008eaa4db
Aokijang <balebodoh@gmail.com>
2018-02-02 15:56:19 +0700

    Perbaiki penamaan di siswa

--- help.php
+++ help.php
@@ -56,7 +56,7 @@
                 <li><a href="about.php"><i class="fa fa-info-circle"></i> About</a></li>
                 <li class="hover"><a href="help.php"><i class="fa fa-question-circle-o"></i> Help</a></li>
                 <li><a href="inbox.php"><i class="fa fa-group"></i> Class</a></li>
-                <li><a href="note.php"><i class="fa fa-bookmark-o"></i> Note</a></li>
+                <li><a href="note.php"><i class="fa fa-inbox"></i> Inbox</a></li>
                 <li><a href="assignment.php"><i class="fa fa-flag-o"></i> Assignment</a></li>
                 <li><a href="quiz.php"><i class="fa fa-gamepad" style="color: white"></i> Quiz</a></li>
                 <li><a href="discussion.php"><i class="fa fa-comments" style="color: white"></i> Discussion</a></li>
